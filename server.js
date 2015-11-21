b537042bee0a859ef06685540eb7199aa764b014
Dennis Zhao <denniszhao@berkeley.edu>
2015-11-21 15:06:40 -0800

    Switches to ws

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 /** Web Socket server for communication between the Controller and the Kobuki accessors. */
 
-var PORT = process.env.port || 3000;
+var WebSocketServer = require("ws").Server;
+var http = require("http");
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 
-var app = require('express')();
-var io = require('socket.io')(PORT);
+app.use(express.static(__dirname + "/"));
 
-io.on('connection', function (socket) {
-    console.log('New socket connected...');
+var server = http.createServer(app);
+server.listen(port);
 
-    socket.on('message', function (msg) {
-        console.log('Moving Kobuki to ' + msg + '...');
+console.log("HTTP server listening on %d...", port);
 
-        io.emit('Received message: ' + msg);
-    });
+var wss = new WebSocketServer({server: server});
+console.log("WebSocket server created...");
+
+wss.on("connection", function(ws) {
+  console.log("New socket connected...");
+
+  ws.on('message', function (msg) {
+      console.log('Moving Kobuki to ' + msg + '...');
+
+      ws.send('ACK: ' + msg);
+  });
 });
