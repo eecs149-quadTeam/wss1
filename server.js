0a23eb12154b3524cef79aae4347c5f170766fdb
Dennis Zhao <denniszhao@berkeley.edu>
2015-11-21 14:53:02 -0800

    Moves to process env port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 /** Web Socket server for communication between the Controller and the Kobuki accessors. */
 
+var PORT = process.env.port || 3000;
+
 var app = require('express')();
-var io = require('socket.io')(3000);
+var io = require('socket.io')(PORT);
 
 io.on('connection', function (socket) {
     console.log('New socket connected...');
